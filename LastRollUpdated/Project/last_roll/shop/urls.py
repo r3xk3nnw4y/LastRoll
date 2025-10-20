b3b8e461a181d8c15ccd399ecb28af97f3e3f57a
USAGSmith <149104140+USAGSmith@users.noreply.github.com>
2025-10-20 15:40:04 -0500

    Removed Outdated Versions
    Removing Outdated Version's so its cleaner

--- LastRollUpdated/Project/last_roll/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .  import views
-
-urlpatterns = [
-    path('', views.home, name='shop-home'),
-    path('about/', views.about, name='shop-about'),
-    path('login/', views.login, name='shop-login'),
-    path('register/', views.register, name='shop-register'),
-]
